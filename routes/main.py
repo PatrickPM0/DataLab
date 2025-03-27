9c0f4617e57b68cfa7e5a34cd360628b2167f5d1
Patrick May <PatrickPM.May@gmail.com>
2025-03-27 14:05:41 -0500

    load in kaggle dataset

--- routes/main.py
+++ routes/main.py
@@ -1,7 +1,14 @@
 from flask import Blueprint, render_template
+import pandas as pd
+import json
+import plotly.express as px
 
 # Define a Blueprint
 main_bp = Blueprint("main", __name__)
+
+
+# Load the dataset
+df = pd.read_csv("data\Student_Mental_Stress_and_Coping_Mechanisms.csv")
 
 
 @main_bp.route("/")
